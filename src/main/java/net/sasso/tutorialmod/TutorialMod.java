f0524ad9b7150da5137b2df65589af00d6044143
Sasso <ricciuti.alessio@hotmail.com>
2024-09-20 18:25:22 +0200

    Just testing

--- src/main/java/net/sasso/tutorialmod/TutorialMod.java
+++ src/main/java/net/sasso/tutorialmod/TutorialMod.java
@@ -19,9 +19,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorial_mod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
